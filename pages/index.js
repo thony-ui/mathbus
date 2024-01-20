ff381dd378ce8e7d4c3e3cd4e49ec11e3861df52
Anthony <anthonyhermanto1@gmail.com>
2024-01-21 00:36:12 +0800

    changes

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div>
         <h1 className="font-bold text-[50px] text-center">NUS Math Bus</h1>
+        <p className="text-center mb-[10px]">Solve for {`"x"`}</p>
         <Datas />
     </div>
   );
